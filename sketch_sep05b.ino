1c2243d2fa63015cc99055a153f2b99c90945a4a
Saikat6-eng <53113450+Saikat6-eng@users.noreply.github.com>
2019-09-17 16:26:51 +0530

    Update sketch_sep05b.ino

--- sketch_sep05b.ino
+++ sketch_sep05b.ino
@@ -1,3 +1,5 @@
+#include <avr/interrupt.h> 
+#include <avr/io.h>
 #include "EnableInterrupt.h"
 #include <EEPROM.h>
 
@@ -43,16 +45,28 @@ volatile uint16_t rc_shared[RC_NUM_CHANNELS]={us_default,us_default,900,us_defau
 
 uint16_t cal_RCusec_max[4]={0x00FF,0x00FF,0x00FF,0x00FF};
 uint16_t cal_RCusec_min[4]={0x0FFF,0x0FFF,0x0FFF,0x0FFF};
+volatile uint8_t i=0;
 
 float cal_MAG[3]={0.0f,0.0f,0.0f};
 float cal_ACCL[3]={0.0f,0.0f,0.0f};
 float cal_GYRO[3]={0.0f,0.0f,0.0f};
 
-char c=0;
 char buf_imu[40]={0};
-
+volatile char buf_isr[40]={0};
 int eeAddress = 0;
 int temp_addr = 16;
+
+ISR(USART_RX_vect)
+{	
+	cli();
+	char c = UDR0;
+	buf_isr[i++]=c;
+	if(c=='\n'){
+	i=buf_isr[i-1]=0;
+	}
+	Serial.println(c);
+	sei();
+}
 
 void rc_read_values() {
   noInterrupts();
@@ -71,12 +85,6 @@ void calc_input(uint8_t channel, uint8_t input_pin) {
     rc_shared[channel] = (uint16_t)((1-alpha)*rc_shared[channel] + (alpha * temp));    
   }
 }
-uint16_t set_filter(uint16_t value_out, uint16_t value_in)
-{
-  value_out=(1-alpha)*value_out + (value_in*alpha);
-  
-return value_out;
-}
 
 void calc_ch1() { calc_input(RC_CH1, RC_CH1_INPUT); }
 void calc_ch2() { calc_input(RC_CH2, RC_CH2_INPUT); }
@@ -89,15 +97,16 @@ void calc_ch8() { calc_input(RC_CH8, RC_CH8_INPUT); }
 
 void setup() {
   
-  DDRC|=0b00001000;
-  
-  char temp=0,i=0;
+DDRC|=0b00001000;
 
-  Serial.begin(SERIAL_PORT_SPEED);
+Serial.begin(SERIAL_PORT_SPEED);
+	
+UCSR0C |= (1 << UCSZ01) | (1 << UCSZ00);
+ // Use 8-bit character sizes 
+UCSR0B |= (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0);
+	
+sei();
  
-  while (!Serial) {
-    ;
-  }
   delay(100);
   pinMode(RC_CH1_INPUT, INPUT);
   pinMode(RC_CH2_INPUT, INPUT);
@@ -121,14 +130,12 @@ void setup() {
   enableInterrupt(RC_CH7_INPUT, calc_ch7, CHANGE);
   enableInterrupt(RC_CH8_INPUT, calc_ch8, CHANGE);
 
-  while(Serial.available()==0);
-  c=Serial.read();
-  while(!((c=='4')||(c=='6')||(c=='8')||(c=='A')))
+  while(!((buf_isr[0]=='4')||(buf_isr[0]=='6')||(buf_isr[0]=='8')||(buf_isr[0]=='A')))
   {
-  c=0;
-  c=Serial.read();
-  if(c=='K')
+  if(buf_isr[0]=='K')
   {
+    eeAddress=0;
+		  
     Calibrate_RCT(void);
     //save the calibration value to eeprom
     delay(10);
@@ -151,7 +158,7 @@ void setup() {
     Serial.println('1');
   }
   
-  else if(c=='R')
+  else if(buf_isr[0]=='R')
   {
     //read the calibration value from eeprom for RC
     eeAddress=0;
@@ -173,43 +180,56 @@ void setup() {
     Serial.print(cal_RCusec_max[RC_CH4]); Serial.println(',');
    
   }
-  else if(c=='I')
+  else if(buf_isr[0]=='I')
   {
     //save the IMU calibration data	  
-	while(temp=Serial.read())
-	{
-	buf_imu[i++]=temp;
-	if(temp=='\n;)
-	   {
-		   buf_imu[i]=0;i=0;
-		   break;
-	   }	
-	}
-	decode_imu_data(buf_imu);
-
+	decode_imu_data(&buf_isr[1]);
+	eeAddress = temp_addr;
+	  
+	    EEPROM.put(eeAddress,cal_MAG[X]);  eeAddress+=sizeof(float);
 	    delay(10);
-	    EEPROM.put(eeAddress,cal_RCusec_max[RC_CH1]);  eeAddress+=sizeof(float);
+	    EEPROM.put(eeAddress,cal_MAG[Y]);  eeAddress+=sizeof(float);
 	    delay(10);
-	    EEPROM.put(eeAddress,cal_RCusec_max[RC_CH2]);  eeAddress+=sizeof(uint16_t);
+	    EEPROM.put(eeAddress,cal_MAG[Z]);  eeAddress+=sizeof(float);
 	    delay(10);
-	    EEPROM.put(eeAddress,cal_RCusec_max[RC_CH3]);  eeAddress+=sizeof(uint16_t);
+	    EEPROM.put(eeAddress,cal_ACCL[X]);  eeAddress+=sizeof(float);
 	    delay(10);
-	    EEPROM.put(eeAddress,cal_RCusec_max[RC_CH4]);  eeAddress+=sizeof(uint16_t);
+	    EEPROM.put(eeAddress,cal_ACCL[Y]);  eeAddress+=sizeof(float);
 	    delay(10);
-	    EEPROM.put(eeAddress,cal_RCusec_min[RC_CH1]);  eeAddress+=sizeof(uint16_t);
+	    EEPROM.put(eeAddress,cal_ACCL[Z]);  eeAddress+=sizeof(float);
 	    delay(10);
-	    EEPROM.put(eeAddress,cal_RCusec_min[RC_CH2]);  eeAddress+=sizeof(uint16_t);
+	    EEPROM.put(eeAddress,cal_GYRO[X]);  eeAddress+=sizeof(float);
 	    delay(10);
-	    EEPROM.put(eeAddress,cal_RCusec_min[RC_CH3]);  eeAddress+=sizeof(uint16_t);
+	    EEPROM.put(eeAddress,cal_GYRO[Y]);  eeAddress+=sizeof(float);
 	    delay(10);
-	    EEPROM.put(eeAddress,cal_RCusec_min[RC_CH4]);  eeAddress+=sizeof(uint16_t);
+	    EEPROM.put(eeAddress,cal_GYRO[Z]);  eeAddress+=sizeof(float);
 	    delay(10);
+	  Serial.print('1');
   }
-  else if(c=='G')
+  else if(buf_isr[0]=='G')
   {
    //read calibration data for IMU 
+	eeAddress = temp_addr;
+	  
+	    EEPROM.get(eeAddress,cal_MAG[X]);  eeAddress+=sizeof(float);
+	    Serial.print(cal_MAG[X]); Serial.print(',');
+	    EEPROM.get(eeAddress,cal_MAG[Y]);  eeAddress+=sizeof(float);
+	    Serial.print(cal_MAG[Y]); Serial.print(',');
+	    EEPROM.get(eeAddress,cal_MAG[Z]);  eeAddress+=sizeof(float);
+	    Serial.print(cal_MAG[Z]); Serial.print(',');
+	    EEPROM.get(eeAddress,cal_ACCL[X]);  eeAddress+=sizeof(float);
+	    Serial.print(cal_ACCL[X]); Serial.print(',');
+	    EEPROM.get(eeAddress,cal_ACCL[Y]);  eeAddress+=sizeof(float);
+	    Serial.print(cal_ACCL[Y]); Serial.print(',');
+	    EEPROM.get(eeAddress,cal_ACCL[Z]);  eeAddress+=sizeof(float);
+	    Serial.print(cal_ACCL[Z]); Serial.print(',');
+	    EEPROM.get(eeAddress,cal_GYRO[X]);  eeAddress+=sizeof(float);
+	    Serial.print(cal_GYRO[X]); Serial.print(',');
+	    EEPROM.get(eeAddress,cal_GYRO[Y]);  eeAddress+=sizeof(float);
+	    Serial.print(cal_GYRO[X]); Serial.print(',');
+	    EEPROM.get(eeAddress,cal_GYRO[Z]);  eeAddress+=sizeof(float);
+	    Serial.print(cal_GYRO[Z]); Serial.println(',');	  
   }
-
   delay(10);
   }
 }
